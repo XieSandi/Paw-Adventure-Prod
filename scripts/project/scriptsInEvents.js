9fc983a1b29605ad1704d64ce78b46e2cd48e0e4
Sandi Pratama <hello@xiesandi.cyou>
2025-03-17 10:55:23 +0700

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,46 @@
+
+
+
+const scriptsInEvents = {
+
+	async Maingame_Event2_Act6(runtime, localVars)
+	{
+		
+	},
+
+	async Mainmenu_Event9_Act2(runtime, localVars)
+	{
+		const currentDate = new Date();
+		console.log(currentDate)
+		runtime.globalVars.TimeStamp = currentDate
+	},
+
+	async Mainmenu_Event9_Act6(runtime, localVars)
+	{
+		console.log("Button CLicked")
+	},
+
+	async Mainmenu_Event10_Act2(runtime, localVars)
+	{
+		console.log("Post Success")
+	},
+
+	async Mainmenu_Event12_Act1(runtime, localVars)
+	{
+		console.log("Button CLicked")
+	},
+
+	async Mainmenu_Event13_Act2(runtime, localVars)
+	{
+		console.log("Update Success")
+	},
+
+	async Mainmenu_Event14_Act2(runtime, localVars)
+	{
+		console.log(runtime.globalVars.UserID)
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
